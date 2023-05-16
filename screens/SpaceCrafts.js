2356e0431682f5ca309035ce9cefa7e94e1a9ef4
AyaanNaha <ayaan.naha26@gmail.com>
2023-05-15 17:57:28 -0700

    Finished project 80

--- screens/SpaceCrafts.js
+++ screens/SpaceCrafts.js
@@ -1,7 +1,51 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, StyleSheet, Image } from 'react-native';
+import axios from 'axios';
 
 export default class SpaceCraftsScreen extends Component {
+    constructor() {
+        super();
+        this.state = {
+            aircrafts: {}
+        }
+    }
+
+    getData=()=> {
+        axios.get("https://ll.thespacedevs.com/2.0.0/config/spacecraft/").then(response => {
+            this.setState({ aircrafts: response.data.results })
+        })
+        .catch(error => {
+            console.log(error.message)
+        })
+    }
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    renderItem = ({item}) => {
+        if (item.agency.image_url === null) {
+            item.agency.image_url = "https://spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com/media/orbiter_images/apollo2520command2fservice2520module_image_20190207032507.jpeg"
+        }
+        return (
+            <View style={styles.container}>
+                <Image
+                    source={{uri: item.agency.image_url}} style={styles.image}
+                ></Image>
+
+                <Text style={{fontWeight:'bold', fontSize:20}}>{item.name}</Text>
+                <Text style={{color:'#696969'}}>{item.agency.name}</Text>
+                <Text>DESCRIPTION</Text>
+                <Text style={{color:'#A9A9A9'}}>{item.agency.description}</Text>
+            </View>
+        )
+        
+    }
+
+    keyExtractor = ({item}, index) => {
+        index.toString();
+    }
+
     render() {
         return (
             <View 
@@ -10,8 +54,34 @@ export default class SpaceCraftsScreen extends Component {
                     justifyContent:'center',
                     alignItems:'center'
                 }}>
-                <Text>Space Crafts Screen</Text>
+                <View style={{flex:0.25, marginTop: 20}}>
+                    <Text style={{fontSize:25}}>Space Crafts</Text>
+                </View>
+                <View style={{flex:0.75}}>
+                    <FlatList
+                        keyExtractor={this.keyExtractor}
+                        data={this.state.aircrafts}
+                        renderItem={this.renderItem}
+                    ></FlatList>
+                </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        borderWidth:1,
+        justifyContent:'center',
+        alignItems:'center',
+        marginBottom:10,
+        elevation:10
+    },
+    image:{
+        width: "100%",
+        height:200,
+        marginTop:15,
+        marginBottom:15,
+        marginRight:10
+    }
+})
